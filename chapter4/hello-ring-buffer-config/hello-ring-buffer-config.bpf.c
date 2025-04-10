addf4727f1398b9011c40c146b458ecb24dccf0c
Viktor Doronin <100450631@alumnos.uc3m.es>
2025-04-10 14:10:35 +0200

    ch4 done(big commit, noob shit yeah ik)

---
+++ chapter4/hello-ring-buffer-config/hello-ring-buffer-config.bpf.c
@@ -0,0 +1,55 @@
+//go:build ignore
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct data_t {     
+   int pid;
+   int uid;
+   char command[16];
+   char message[12];
+};
+
+struct user_msg_t {
+   char message[12];
+};
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __type(key, __u32);
+  __type(value, struct user_msg_t);
+  __uint(max_entries, 10240);
+} config SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_RINGBUF);
+  __uint(max_entries, 4096);
+} output SEC(".maps");
+
+ 
+
+SEC("kprobe")
+int hello(void *ctx) {
+   struct data_t data = {}; 
+   char message[12] = "Hello World";
+   struct user_msg_t *p;
+
+   data.pid = bpf_get_current_pid_tgid() >> 32;
+   data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+   bpf_get_current_comm(&data.command, sizeof(data.command));
+
+   p = bpf_map_lookup_elem(&config, &data.uid);
+   if (p != 0) {
+      bpf_probe_read_kernel(&data.message, sizeof(data.message), p->message);       
+   } else {
+      bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
+   }
+
+   bpf_ringbuf_output(&output, &data, sizeof(data), 0);
+ 
+   return 0;
+}
--- chapter4/hello-ring-buffer-config/hello-ring-buffer-config.go
+++ chapter4/hello-ring-buffer-config/hello-ring-buffer-config.go
@@ -1,14 +1,63 @@
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go hello-ring-buffer-config hello-ring-buffer-config.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go hello_ring_buffer_config hello-ring-buffer-config.bpf.c
 package main
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+
+
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+type bpfOutput struct{
+	Pid int32
+	Uid int32
+	Command [16]byte
+	Message [12]byte
+}
+
 func main() {
-    // Remove resource limits for kernels <5.11.
-    if err := rlimit.RemoveMemlock(); err != nil { 
-        log.Fatal("Removing memlock:", err)
-    }
-// Load the compiled eBPF ELF and load it into the kernel.
-    var objs hello-ring-buffer-configObjects
-    if err := loadHello-ring-buffer-configObjects(&objs, nil); err != nil {
-        log.Fatal("Loading eBPF objects:", err)
-    }
-    defer objs.Close()
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil { 
+		log.Fatal("Removing memlock:", err)
+	}
+	// Load the compiled eBPF ELF and load it into the kernel.
+	var objs hello_ring_buffer_configObjects
+	if err := loadHello_ring_buffer_configObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects:", err)
+	}
+	defer objs.Close()
+	
+	kp, err := link.Kprobe("sys_execve", objs.Hello, nil)
+	if err != nil {
+		log.Fatalf("opening kprobe: %s", err)
+	}
+	defer kp.Close()
+
+	ringrd, err:=ringbuf.NewReader(objs.Output)
+	if err!=nil {
+		log.Fatal(err)
+	}
+	defer ringrd.Close()
+
+	log.Println("Waiting for events..")
+	
+	var output bpfOutput
+
+	for{
+		fmt.Print("entering cycle")
+		record, err := ringrd.Read()
+		if err != nil {
+			panic(err)
+		}
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &output); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+		fmt.Printf("Waiting for read")
+		fmt.Printf("%d %d %s %s\n", output.Uid, output.Pid, output.Command, output.Message)
+	}
 }
